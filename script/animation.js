d9e26d57c36bac359ce9460dacd763f9f42cda1d
47585io <442817485>
2024-08-03 03:05:34 +0000

    new file:   res/animation/hero_run.json 	deleted:    res/animation/hero_run.txt 	modified:   script/animation.js 	modified:   script/files.js

--- script/animation.js
+++ script/animation.js
@@ -1,6 +1,27 @@
 class Animation
 {
-    mAnimationListener = null
+    /**
+     * 
+     * @param {AnimationFrames} frames 
+     */
+    constructor(frames){
+        this.mAnimationFrames = frames
+        this.mAnimationListener = null
+        this.mFrameDuartion = 10
+    }
+}
+
+class AnimationFrames
+{
+    /**
+     * 
+     * @param {CanvasImageSource} image 
+     * @param {Array<Rect>} bounds 
+     */
+    constructor(image, bounds){
+        this.mCurrentImage = image
+        this.mFrameBounds = bounds
+    }
 }
 
 class AnimationListener
--- script/files.js
+++ script/files.js
@@ -31,7 +31,9 @@ class FileUtils
     /**
      * load image file from pictures directory
      * @param {string} path The path to the image file
-     * @returns {Promise<CanvasImageSource>} A Promise that resolves to a CanvasImageSource
+     * @returns {Promise<CanvasImageSource>} 
+     *      A Promise that resolves to a CanvasImageSource, 
+     *      Handle image in then(), and must return image for next promise 
      */
     static loadImage(path) 
     {
@@ -61,7 +63,6 @@ class FileUtils
             // the image is already loaded, return resolved Promise
             return Promise.resolve(data)
         }
-
         // Handle any other unexpected cases
         return Promise.reject(new Error('Invalid data type for ' + path));
     }
@@ -88,8 +89,73 @@ class FileUtils
             });
     }
 
+    /**
+     * 
+     * @param {string} path 
+     * @returns {AnimationFrames}
+     */
     static loadAnimation(path)
     {
-       
+        path = Res.animation_directory + path
+        let data = this.mPathToData.get(path)
+
+        if(data === undefined){
+            let loadPromise = this.newFramesPromise(path)
+                .then(frames => {
+                    this.mPathToData.set(path, frames)
+                    return frames
+                })
+            this.mPathToData.set(path, loadPromise)
+            return loadPromise
+        }
+
+        if(data instanceof Promise){
+            return data
+        }
+        else if(data instanceof AnimationFrames){
+            return Promise.resolve(data)
+        }
+        return Promise.reject(new Error('Invalid data type for ' + path));
+    }
+
+    /**
+     * 
+     * @param {string} path 
+     * @returns {Promise<AnimationFrames>}
+     */
+    static newFramesPromise(path)
+    {
+        this.loadTextFile(path)
+            .then(text => {
+                let data = JSON.parse(text)
+                this.loadImage(data.imagePath)
+                    .then(image => {
+                        let boundsArray = new Array()
+                        for(let bounds of data.frameBounds){
+                            let rect = new Rect(bounds[0], bounds[1], bounds[2], bounds[3])
+                            boundsArray.push(rect)
+                        }
+                        return new AnimationFrames(image, boundsArray)
+                    })
+            })
+    }
+
+    /**
+     * 
+     * @param {string} path 
+     * @returns {Promise<string>}
+     */
+    static loadTextFile(path) 
+    {
+        return fetch(path)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text(); 
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
     }
 }
